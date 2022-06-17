32486091555883c71dc377162c4b6a932dbbf437
chern <christopher.t.hern@gmail.com>
2022-06-17 09:15:58 -0500

    feat: added RunAll() method for interactive sessions

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -3,45 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/codered-by-ec-council/Hands-on-Network-Programming-with-Go/pkg/devcon"
 )
 
 func main() {
 	target := flag.String("target", "127.0.0.1", "target against which to run a command")
-	cmd := flag.String("cmd", "", "command to run against target device")
+	cmdFile := flag.String("cmdfile", "", "command filename")
 	flag.Parse()
 	client := devcon.NewClient(*target)
-	output, err := client.Run(*cmd)
+	f, err := os.Open(*cmdFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	bs, err := io.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cmds := strings.Split(string(bs), "\n")
+	output, err := client.RunAll(cmds)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(output)
 }
-
-// func run(target, cmd string) (string, error) {
-// 	cfg := &ssh.ClientConfig{
-// 		User: os.Getenv("SSH_USER"),
-// 		Auth: []ssh.AuthMethod{
-// 			ssh.Password(os.Getenv("SSH_PASSWORD")),
-// 		},
-// 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-// 		Timeout:         time.Second * 5,
-// 	}
-// 	client, err := ssh.Dial("tcp", fmt.Sprintf("%v:22", target), cfg)
-// 	if err != nil {
-// 		return "", errors.Wrap(err, "dial failed")
-// 	}
-// 	defer client.Close()
-// 	session, err := client.NewSession()
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	defer session.Close()
-// 	output, err := session.CombinedOutput(cmd)
-// 	if err != nil {
-// 		return "", err
-// 	}
-// 	return string(output), nil
-// }
--- pkg/devcon/devcon.go
+++ pkg/devcon/devcon.go
@@ -2,6 +2,7 @@ package devcon
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"time"
 
@@ -44,4 +45,40 @@ func (c *sshClient) Run(cmd string) (string, error) {
 		return "", err
 	}
 	return string(output), nil
+}
+
+func (c *sshClient) RunAll(cmds []string) (string, error) {
+	client, err := ssh.Dial("tcp", fmt.Sprintf("%v:22", c.target), c.cfg)
+	if err != nil {
+		return "", errors.Wrap(err, "dial failed")
+	}
+	defer client.Close()
+	session, err := client.NewSession()
+	if err != nil {
+		return "", err
+	}
+	defer session.Close()
+	wc, err := session.StdinPipe()
+	if err != nil {
+		return "", err
+	}
+	defer wc.Close()
+	r, err := session.StdoutPipe()
+	if err != nil {
+		return "", err
+	}
+	if err := session.Shell(); err != nil {
+		return "", err
+	}
+	for _, cmd := range cmds {
+		_, err := fmt.Fprintf(wc, "%s\n", cmd)
+		if err != nil {
+			return "", err
+		}
+	}
+	bs, err := io.ReadAll(r)
+	if err != nil {
+		return "", err
+	}
+	return string(bs), nil
 }
