767f2c31846a75fad331637aa2fadf20ba3e153f
chern <christopher.t.hern@gmail.com>
2022-06-18 09:36:52 -0500

    added examples to parse data line-by-line (splitting) and using regexp

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/codered-by-ec-council/Hands-on-Network-Programming-with-Go/pkg/devcon"
@@ -19,9 +22,44 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	output, err := client.Run("show version")
+	output, err := client.Run("show isis adjacency")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(output)
+	isisAdj := parseRegex(output)
+	for k, v := range isisAdj {
+		fmt.Printf("%v: %v\n", k, v)
+	}
+}
+
+func parseLineSplit(in string) map[string]string {
+	var keys []string
+	isisAdj := make(map[string]string)
+	s := bufio.NewScanner(strings.NewReader(in))
+	for s.Scan() {
+		ln := s.Text()
+		if strings.Contains(ln, "Interface") {
+			fields := strings.Fields(ln)
+			for i := 0; i < 4; i++ {
+				keys = append(keys, fields[i])
+			}
+		}
+		fields := strings.Fields(ln)
+		for i := 0; i < 4; i++ {
+			isisAdj[keys[i]] = fields[i]
+		}
+	}
+	return isisAdj
+}
+
+func parseRegex(in string) map[string]string {
+	isisAdj := make(map[string]string)
+	pat := regexp.MustCompile(`(?P<intf>[fg]e-\d\/\d/\d\.\d+)\s{2,}(?P<hostname>[a-zA-Z_0-9]+)\s{2,}(?P<level>\d)\s{2,}(?P<state>[a-zA-Z]+)`)
+	match := pat.FindStringSubmatch(in)
+	for idx, name := range pat.SubexpNames() {
+		if idx > 0 && idx <= len(match) {
+			isisAdj[name] = match[idx]
+		}
+	}
+	return isisAdj
 }
