93ed995a94dcac06b39d5ded17eda434f7508a23
chern <christopher.t.hern@gmail.com>
2022-06-18 07:43:30 -0500

    cleaned up exp dir

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -1,44 +1,25 @@
 package main
 
 import (
-	"flag"
 	"fmt"
-	"io"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 	"time"
 
 	"github.com/codered-by-ec-council/Hands-on-Network-Programming-with-Go/pkg/devcon"
 )
 
 func main() {
-	target := flag.String("target", "127.0.0.1", "target against which to run a command")
-	cmdFile := flag.String("cmdfile", "", "command filename")
-	flag.Parse()
-	// set hostKey
-	knownhosts := filepath.Join(os.Getenv("HOME"), ".ssh", "known_hosts")
 	client, err := devcon.NewClient(
-		"testkey",
-		*target,
-		devcon.SetTimeout(time.Second*1),
-		devcon.SetKey(filepath.Join(os.Getenv("HOME"), "Desktop", "id_rsa")),
-		devcon.SetHostKeyCallback(knownhosts),
+		os.Getenv("SSH_USER"),
+		"labsrx",
+		devcon.SetPassword(os.Getenv("SSH_PASSWORD")),
+		devcon.SetTimeout(time.Second*5),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
-	f, err := os.Open(*cmdFile)
-	if err != nil {
-		log.Fatal(err)
-	}
-	bs, err := io.ReadAll(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-	cmds := strings.Split(string(bs), "\n")
-	output, err := client.RunAll(cmds)
+	output, err := client.Run("show version")
 	if err != nil {
 		log.Fatal(err)
 	}
