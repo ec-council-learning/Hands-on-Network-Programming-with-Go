d7c932504e045872e526cbc48d09cf6cd13f4be3
chern <christopher.t.hern@gmail.com>
2022-06-23 10:38:51 -0500

    added specific route cmd and refactored run and unmarshal

--- cmd/exp/main.go
+++ cmd/exp/main.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	isisAdj := cmdrunner.ISISAdjacencyRpcReply{Target: "labsrx", ExpectedNeighbor: "lab_srx10"}
-	if err := isisAdj.Run(); err != nil {
+	// isisAdj := cmdrunner.ISISAdjacencyRpcReply{Target: "labsrx", ExpectedNeighbor: "lab_srx10"}
+	// if err := isisAdj.Run(); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// err := isisAdj.Compare()
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	sr := cmdrunner.SpecificRouteRpcReply{Target: "labsrx", ExpectedNextHop: "192.168.0.1"}
+	if err := sr.Run(); err != nil {
 		log.Fatal(err)
 	}
-	err := isisAdj.Compare()
-	if err != nil {
+	if err := sr.Compare(); err != nil {
 		log.Println(err)
 	}
 }
--- pkg/cmdrunner/commands.go
+++ pkg/cmdrunner/commands.go
@@ -23,6 +23,22 @@ func (ce *CompareError) Error() string {
 	return fmt.Sprintf("compare error - got: %q expected: %q", ce.got, ce.expected)
 }
 
+func getOutput(target, cmd string) (string, error) {
+	client, err := devcon.NewClient(user, target, devcon.SetKey(key))
+	if err != nil {
+		return "", err
+	}
+	output, err := client.Run(cmd)
+	if err != nil {
+		return "", err
+	}
+	return output, nil
+}
+
+func unmarshal(bs []byte, data interface{}) error {
+	return xml.Unmarshal(bs, data)
+}
+
 type ISISAdjacencyRpcReply struct {
 	Target                   string
 	ExpectedNeighbor         string
@@ -47,15 +63,11 @@ func (ia *ISISAdjacencyRpcReply) unmarshal(bs []byte) error {
 
 func (ia *ISISAdjacencyRpcReply) Run() error {
 	cmd := "show isis adjacency | display xml"
-	client, err := devcon.NewClient(user, ia.Target, devcon.SetKey(key))
+	output, err := getOutput(ia.Target, cmd)
 	if err != nil {
 		return err
 	}
-	output, err := client.Run(cmd)
-	if err != nil {
-		return err
-	}
-	if err := ia.unmarshal([]byte(output)); err != nil {
+	if err := unmarshal([]byte(output), ia); err != nil {
 		return err
 	}
 	return nil
@@ -65,6 +77,69 @@ func (ia *ISISAdjacencyRpcReply) Compare() error {
 	gotSystemName := ia.IsisAdjacencyInformation.IsisAdjacency.SystemName
 	if gotSystemName != ia.ExpectedNeighbor {
 		return &CompareError{got: gotSystemName, expected: ia.ExpectedNeighbor}
+	}
+	return nil
+}
+
+type SpecificRouteRpcReply struct {
+	Target           string
+	Prefix           string
+	ExpectedNextHop  string
+	RouteInformation struct {
+		Text       string `xml:",chardata"`
+		Xmlns      string `xml:"xmlns,attr"`
+		RouteTable struct {
+			Text               string `xml:",chardata"`
+			TableName          string `xml:"table-name"`
+			DestinationCount   string `xml:"destination-count"`
+			TotalRouteCount    string `xml:"total-route-count"`
+			ActiveRouteCount   string `xml:"active-route-count"`
+			HolddownRouteCount string `xml:"holddown-route-count"`
+			HiddenRouteCount   string `xml:"hidden-route-count"`
+			Rt                 struct {
+				Text          string `xml:",chardata"`
+				Style         string `xml:"style,attr"`
+				RtDestination string `xml:"rt-destination"`
+				RtEntry       struct {
+					Text          string `xml:",chardata"`
+					ActiveTag     string `xml:"active-tag"`
+					CurrentActive string `xml:"current-active"`
+					LastActive    string `xml:"last-active"`
+					ProtocolName  string `xml:"protocol-name"`
+					Preference    string `xml:"preference"`
+					Age           struct {
+						Text    string `xml:",chardata"`
+						Seconds string `xml:"seconds,attr"`
+					} `xml:"age"`
+					Metric string `xml:"metric"`
+					Nh     struct {
+						Text            string `xml:",chardata"`
+						SelectedNextHop string `xml:"selected-next-hop"`
+						To              string `xml:"to"`
+						Via             string `xml:"via"`
+					} `xml:"nh"`
+				} `xml:"rt-entry"`
+			} `xml:"rt"`
+		} `xml:"route-table"`
+	} `xml:"route-information"`
+}
+
+func (sr *SpecificRouteRpcReply) Run() error {
+	cmd := fmt.Sprintf("show route %s | display xml", sr.Prefix)
+	output, err := getOutput(sr.Target, cmd)
+	if err != nil {
+		return err
+	}
+	if err := unmarshal([]byte(output), sr); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sr *SpecificRouteRpcReply) Compare() error {
+	gotNextHop := sr.RouteInformation.RouteTable.Rt.RtEntry.Nh.To
+	if gotNextHop != sr.ExpectedNextHop {
+		return &CompareError{got: gotNextHop, expected: sr.ExpectedNextHop}
 	}
 	return nil
 }
