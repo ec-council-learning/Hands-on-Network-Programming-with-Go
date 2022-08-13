665bced9a0b6ba8614e7f2cbf3093e94b25b39dd
chern <christopher.t.hern@gmail.com>
2022-08-13 09:40:27 -0500

    created templates and some template logic

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,86 +1,89 @@
 package main
 
 import (
+	"embed"
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/pkg/errors"
 )
+
+//go:embed templates/*
+var content embed.FS
+
+var templateCache = map[string]*template.Template{}
 
 func main() {
 	websocket := flag.String("websocket", "localhost:8080", "socket on which to listen for incoming connections")
+	tc, err := newTemplateCache("templates")
+	if err != nil {
+		log.Println(err)
+	}
+	templateCache = tc
 	http.HandleFunc("/", handleHome)
+	http.HandleFunc("/vendors", handleVendors)
 	log.Println("starting web server on", *websocket)
 	http.ListenAndServe(*websocket, nil)
 }
 
 func handleHome(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(`
-	<!doctype html>
-	<html lang="en">
-	  <head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Bootstrap demo</title>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-	  </head>
-	  <body>
-	  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-	  <div class="container-fluid">
-		<a class="navbar-brand" href="/">Gopher Engineering</a>
-		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-		  <span class="navbar-toggler-icon"></span>
-		</button>
-		<div class="collapse navbar-collapse" id="navbarSupportedContent">
-		  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-			<li class="nav-item">
-			  <a class="nav-link active" aria-current="page" href="/">Home</a>
-			</li>
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-				Vendors
-			  </a>
-			  <ul class="dropdown-menu">
-				<li><a class="dropdown-item" href="/vendors/new">New</a></li>
-				<li><a class="dropdown-item" href="/vendors">List</a></li>
-			  </ul>
-			</li>
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-				Models
-			  </a>
-			  <ul class="dropdown-menu">
-				<li><a class="dropdown-item" href="/models/new">New</a></li>
-				<li><a class="dropdown-item" href="/models">List</a></li>
-			  </ul>
-			</li>
-			<li class="nav-item dropdown">
-			  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-				Devices
-			  </a>
-			  <ul class="dropdown-menu">
-				<li><a class="dropdown-item" href="/devices/new">New</a></li>
-				<li><a class="dropdown-item" href="/devices">List</a></li>
-			  </ul>
-			</li>
-		  </ul>
-		  <form class="d-flex" role="search">
-			<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-			<button class="btn btn-outline-success" type="submit">Search</button>
-		  </form>
-		</div>
-	  </div>
-	</nav>
-	<div class="container py-4">
-		<div class="p-5 mb-4 bg-light rounded-3">
-			<div class="container-fluid py-5">
-				<h1 class="display-5 fw-bold">Gopher Engineering</h1>
-				<p class="col-md-8 fs-4">Using a series of utilities, you can create this jumbotron, just like the one in previous versions of Bootstrap. Check out the examples below for how you can remix and restyle it to your liking.</p>
-				<button class="btn btn-primary btn-lg" type="button">Docs</button>
-			</div>
-		</div>
-	</div>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-	</body>
-	</html>
-	`))
+	ts, ok := templateCache["home.page.tmpl"]
+	if !ok {
+		http.Error(w, fmt.Errorf("The template %s does not exist", "home").Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(ts.Name())
+	fmt.Println(ts.DefinedTemplates())
+	if err := ts.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func handleVendors(w http.ResponseWriter, r *http.Request) {
+	ts, ok := templateCache["vendors.page.tmpl"]
+	if !ok {
+		http.Error(w, fmt.Errorf("The template %s does not exist", "vendors").Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := ts.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+var functions = template.FuncMap{}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := content.ReadDir(dir)
+	if err != nil {
+		return nil, errors.Wrap(err, "ReadDir failed")
+	}
+	for _, page := range pages {
+		name := filepath.Base(page.Name())
+		ts, err := template.New(name).Funcs(functions).ParseFS(content, fmt.Sprintf("templates/%v", name))
+		if err != nil {
+			return nil, errors.Wrap(err, "ParseFS failed")
+		}
+		ts, err = ts.ParseFS(content, "templates/*.layout.tmpl")
+		if err != nil {
+			return nil, errors.Wrap(err, "ParseFS failed")
+		}
+		cache[name] = ts
+	}
+	return cache, nil
+}
+
+func render(w http.ResponseWriter, r *http.Request, name string) {
+	ts, ok := templateCache[name]
+	if !ok {
+		http.Error(w, fmt.Errorf("The template %s does not exist", name).Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := ts.Execute(w, nil); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
