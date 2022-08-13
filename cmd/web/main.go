a9a2bada53425baf07c65dfaec7a9117579499df
chern <christopher.t.hern@gmail.com>
2022-08-13 09:49:43 -0500

    added an app struct for dependency management

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,57 +1,63 @@
 package main
 
 import (
+	"context"
 	"embed"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"text/template"
+	"time"
 
+	"github.com/codered-by-ec-council/Hands-on-Network-Programming-with-Go/pkg/inventory"
+	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/pkg/errors"
 )
 
 //go:embed templates/*
 var content embed.FS
 
-var templateCache = map[string]*template.Template{}
+type application struct {
+	templateCache    map[string]*template.Template
+	inventoryService *inventory.Service
+}
 
 func main() {
 	websocket := flag.String("websocket", "localhost:8080", "socket on which to listen for incoming connections")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	dbpool, err := pgxpool.Connect(ctx, os.Getenv("DSN"))
+	if err != nil {
+		log.Println(err)
+	}
+	defer dbpool.Close()
+	if err := dbpool.Ping(ctx); err != nil {
+		log.Println("db conn failed:", err)
+	}
+	fmt.Println("db connected successfully")
 	tc, err := newTemplateCache("templates")
 	if err != nil {
 		log.Println(err)
 	}
-	templateCache = tc
-	http.HandleFunc("/", handleHome)
-	http.HandleFunc("/vendors", handleVendors)
+	app := application{
+		templateCache:    tc,
+		inventoryService: inventory.NewService(dbpool),
+	}
+	http.HandleFunc("/", app.handleHome)
+	http.HandleFunc("/vendors", app.handleVendors)
 	log.Println("starting web server on", *websocket)
 	http.ListenAndServe(*websocket, nil)
 }
 
-func handleHome(w http.ResponseWriter, r *http.Request) {
-	ts, ok := templateCache["home.page.tmpl"]
-	if !ok {
-		http.Error(w, fmt.Errorf("The template %s does not exist", "home").Error(), http.StatusInternalServerError)
-		return
-	}
-	fmt.Println(ts.Name())
-	fmt.Println(ts.DefinedTemplates())
-	if err := ts.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func (app *application) handleHome(w http.ResponseWriter, r *http.Request) {
+	app.render(w, "home.page.tmpl")
 }
 
-func handleVendors(w http.ResponseWriter, r *http.Request) {
-	ts, ok := templateCache["vendors.page.tmpl"]
-	if !ok {
-		http.Error(w, fmt.Errorf("The template %s does not exist", "vendors").Error(), http.StatusInternalServerError)
-		return
-	}
-	if err := ts.Execute(w, nil); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func (app *application) handleVendors(w http.ResponseWriter, r *http.Request) {
+	app.render(w, "vendors.page.tmpl")
 }
 
 var functions = template.FuncMap{}
@@ -77,8 +83,8 @@ func newTemplateCache(dir string) (map[string]*template.Template, error) {
 	return cache, nil
 }
 
-func render(w http.ResponseWriter, r *http.Request, name string) {
-	ts, ok := templateCache[name]
+func (app *application) render(w http.ResponseWriter, name string) {
+	ts, ok := app.templateCache[name]
 	if !ok {
 		http.Error(w, fmt.Errorf("The template %s does not exist", name).Error(), http.StatusInternalServerError)
 		return
