6fc61a2d87a0ead13f87e5d3909ae0347d40de59
chern <christopher.t.hern@gmail.com>
2022-08-14 11:16:45 -0500

    added logic to delete [vendors, models, devices]

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -69,6 +69,56 @@ func (app *application) handleVendorUpdate(w http.ResponseWriter, r *http.Reques
 	app.render(w, "vendors.page.tmpl", vendors)
 }
 
+func (app *application) handleVendor(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
+		return
+	}
+	vendor, err := app.inventoryService.VendorRepo.GetByID(id)
+	if err != nil {
+		http.Error(w, errors.New("vendor is not in DB").Error(), http.StatusBadRequest)
+		return
+	}
+	app.render(w, "vendors_update.page.tmpl", vendor)
+}
+
+func (app *application) handleVendors(w http.ResponseWriter, r *http.Request) {
+	vendors, err := app.inventoryService.VendorRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "vendors.page.tmpl", vendors)
+}
+
+func (app *application) handleVendorDelete(w http.ResponseWriter, r *http.Request) {
+	log.Println("hit vendor delete handler")
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, errors.New("couldn't parse form").Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
+		return
+	}
+	if err := app.inventoryService.VendorRepo.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	vendors, err := app.inventoryService.VendorRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "vendors.page.tmpl", vendors)
+}
+
 func (app *application) handleModelsNew(w http.ResponseWriter, r *http.Request) {
 	vendors, err := app.inventoryService.VendorRepo.GetAll()
 	if err != nil {
@@ -110,73 +160,6 @@ func (app *application) handleModelsAdd(w http.ResponseWriter, r *http.Request) 
 	app.render(w, "models.page.tmpl", models)
 }
 
-func (app *application) handleDeviceNew(w http.ResponseWriter, r *http.Request) {
-	models, err := app.inventoryService.ModelRepo.GetAll()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	app.render(w, "devices_new.page.tmpl", models)
-}
-
-func (app *application) handleDeviceAdd(w http.ResponseWriter, r *http.Request) {
-	log.Println("hit device add handler")
-	if r.Method != http.MethodPost {
-		http.Error(w, errors.New("method should be post").Error(), http.StatusBadRequest)
-		return
-	}
-	if err := r.ParseForm(); err != nil {
-		http.Error(w, errors.New("couldn't parse form").Error(), http.StatusBadRequest)
-		return
-	}
-	idStr := r.FormValue("model_id")
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
-		return
-	}
-	newDevice := models.Device{
-		Hostname: r.FormValue("hostname"),
-		IPv4:     r.FormValue("ip"),
-		Model:    models.Model{ID: id},
-	}
-	if err := app.inventoryService.DeviceRepo.New(newDevice); err != nil {
-		http.Error(w, errors.New("couldn't add model to DB").Error(), http.StatusBadRequest)
-		return
-	}
-	devices, err := app.inventoryService.DeviceRepo.GetAll()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	app.render(w, "devices.page.tmpl", devices)
-}
-
-func (app *application) handleVendor(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	idStr := vars["id"]
-	id, err := strconv.Atoi(idStr)
-	if err != nil {
-		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
-		return
-	}
-	vendor, err := app.inventoryService.VendorRepo.GetByID(id)
-	if err != nil {
-		http.Error(w, errors.New("vendor is not in DB").Error(), http.StatusBadRequest)
-		return
-	}
-	app.render(w, "vendors_update.page.tmpl", vendor)
-}
-
-func (app *application) handleVendors(w http.ResponseWriter, r *http.Request) {
-	vendors, err := app.inventoryService.VendorRepo.GetAll()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	app.render(w, "vendors.page.tmpl", vendors)
-}
-
 func (app *application) handleModels(w http.ResponseWriter, r *http.Request) {
 	models, err := app.inventoryService.ModelRepo.GetAll()
 	if err != nil {
@@ -184,15 +167,6 @@ func (app *application) handleModels(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	app.render(w, "models.page.tmpl", models)
-}
-
-func (app *application) handleDevices(w http.ResponseWriter, r *http.Request) {
-	devices, err := app.inventoryService.DeviceRepo.GetAll()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	app.render(w, "devices.page.tmpl", devices)
 }
 
 func (app *application) handleModel(w http.ResponseWriter, r *http.Request) {
@@ -258,6 +232,82 @@ func (app *application) handleModelUpdate(w http.ResponseWriter, r *http.Request
 	app.render(w, "models.page.tmpl", models)
 }
 
+func (app *application) handleModelDelete(w http.ResponseWriter, r *http.Request) {
+	log.Println("hit model delete handler")
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, errors.New("couldn't parse form").Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
+		return
+	}
+	if err := app.inventoryService.ModelRepo.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	models, err := app.inventoryService.ModelRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "models.page.tmpl", models)
+}
+
+func (app *application) handleDeviceNew(w http.ResponseWriter, r *http.Request) {
+	models, err := app.inventoryService.ModelRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "devices_new.page.tmpl", models)
+}
+
+func (app *application) handleDeviceAdd(w http.ResponseWriter, r *http.Request) {
+	log.Println("hit device add handler")
+	if r.Method != http.MethodPost {
+		http.Error(w, errors.New("method should be post").Error(), http.StatusBadRequest)
+		return
+	}
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, errors.New("couldn't parse form").Error(), http.StatusBadRequest)
+		return
+	}
+	idStr := r.FormValue("model_id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
+		return
+	}
+	newDevice := models.Device{
+		Hostname: r.FormValue("hostname"),
+		IPv4:     r.FormValue("ip"),
+		Model:    models.Model{ID: id},
+	}
+	if err := app.inventoryService.DeviceRepo.New(newDevice); err != nil {
+		http.Error(w, errors.New("couldn't add model to DB").Error(), http.StatusBadRequest)
+		return
+	}
+	devices, err := app.inventoryService.DeviceRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "devices.page.tmpl", devices)
+}
+
+func (app *application) handleDevices(w http.ResponseWriter, r *http.Request) {
+	devices, err := app.inventoryService.DeviceRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "devices.page.tmpl", devices)
+}
+
 func (app *application) handleDevice(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	idStr := vars["id"]
@@ -312,6 +362,31 @@ func (app *application) handleDeviceUpdate(w http.ResponseWriter, r *http.Reques
 	}
 	if err := app.inventoryService.DeviceRepo.Update(device); err != nil {
 		http.Error(w, errors.New("couldn't update vendor in the DB").Error(), http.StatusBadRequest)
+		return
+	}
+	devices, err := app.inventoryService.DeviceRepo.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	app.render(w, "devices.page.tmpl", devices)
+}
+
+func (app *application) handleDeviceDelete(w http.ResponseWriter, r *http.Request) {
+	log.Println("hit device delete handler")
+	if err := r.ParseForm(); err != nil {
+		http.Error(w, errors.New("couldn't parse form").Error(), http.StatusBadRequest)
+		return
+	}
+	vars := mux.Vars(r)
+	idStr := vars["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, errors.New("id is not an int").Error(), http.StatusBadRequest)
+		return
+	}
+	if err := app.inventoryService.DeviceRepo.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	devices, err := app.inventoryService.DeviceRepo.GetAll()
