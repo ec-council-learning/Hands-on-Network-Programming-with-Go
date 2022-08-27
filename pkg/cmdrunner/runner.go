051ebe93288f521c477caebf9caa4b1c4b9c8e06
chern <christopher.t.hern@gmail.com>
2022-08-27 10:58:33 -0500

    added comments to runner

--- pkg/cmdrunner/runner.go
+++ pkg/cmdrunner/runner.go
@@ -2,11 +2,15 @@ package cmdrunner
 
 import "fmt"
 
+// Runner provides the neccessary methods to
+// collect and compare data from a target device.
 type Runner interface {
 	Run() error
 	Compare() error
 }
 
+// Stepper steps through a list of runners, grabbing and comparing
+// the received data while validating success.
 func Stepper(cmds []Runner) error {
 	for _, cmd := range cmds {
 		fmt.Printf("[%v] running\n", cmd)
